d4c6365a324a4166ab877948e2db4b471c9c2eea
Kate <kate.voorhes@gmail.com>
2018-11-16 14:14:23 -0500

    adding new messages works woopee

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const publicDir = $path.resolve("./public");
-const messagesDir = $path.resolve("./messages");
+const messagesDir = $path.resolve("./chat");
 app.use(express.static("public"));
 app.listen(port, () => console.log(`Chat app listening on port ${port}!!!`));
 
@@ -16,23 +16,27 @@ app.get("/", (request, response) => {
   response.sendFile($path.join(publicDir, "main.html"));
 });
 
-app.post(
-  "/rooms/mainroom/messages",
-  express.urlencoded({ extended: false }),
-  (request, response) => {
+app.post("/chat", express.urlencoded({ extended: false }), (request, response) => {
     console.log(request.body);
     createMessage(nextMessageId(), request.body, response);
   }
 );
-app.get('/rooms/mainroom/messages', (request, response) => {
+app.get('/chat', (request, response) => {
   response.sendFile($path.join(publicDir, 'main.html'))
 })
-app.get('/rooms/mainroom/.json', (request, response) => {
-  let messages = allMessages();
-  let data = JSON.stringify(messages);
+
+app.get('/chat.json', (request, response) => {
+  let chat = allMessages();
+  let data = JSON.stringify(chat);
   response.type('application/json').send(data);
 })
 
+app.get('/room-name', (request, response) => {
+  response.sendFile($path.join(publicDir, 'room-name.html'))
+
+})
+
+// note: need to write function if NO messages currently.
 function createMessage(messageId, params, response) {
   let today = new Date();
   let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -48,7 +52,7 @@ function createMessage(messageId, params, response) {
     if (err) {
       response.status(500).send(err);
     } else {
-      response.redirect("/messages");
+      response.redirect("/chat");
     }
   });
 }
@@ -61,15 +65,17 @@ function allMessages() {
 }
 
 function nextMessageId() {
-  let messages = allMessages();
+  let chat = allMessages();
 
   // find the highest id...
-  let id = messages[messages.length - 1].id;
+  let id = chat[chat.length - 1].id;
 
   // ...and pick a higher one
   let messageId = id + 1;
   return messageId;
 }
+
+
 
 //app.get('/main', (request, response) => {
 //let htmlFile = $path.join(publicDir, "main.html");
